0fa1243c73697c91656718e18a4db1347c447987
zeyadsleem <zeyadsleem1@gmail.com>
2024-05-19 00:03:19 +0300

    nvim: make cmdline in the cecnter of screen

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,16 +1,27 @@
 return {
+  -- add noice.nvim
   "folke/noice.nvim",
-  opts = {
-    presets = {
-      lsp_doc_border = true,
-    },
-    lsp = {
-      progress = {
+  config = function()
+    require("noice").setup({
+      presets = {
+        lsp_doc_border = true,
+      },
+      lsp = {
+        progress = {
+          enabled = true,
+        },
+      },
+      messages = {
         enabled = true,
       },
-    },
-    messages = {
-      enabled = true,
-    },
+    })
+  end,
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
   },
 }
