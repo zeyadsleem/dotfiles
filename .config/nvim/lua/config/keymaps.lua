0a41bb569ddec8638716be7edd61b88c9e4a5379
zeyadsleem <zeyadsleem1@gmail.com>
2024-08-23 10:39:44 +0300

    rmeove unused lualine, and esc jj keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- use `vim.keymap.set` instead
 local map = LazyVim.safe_keymap_set
+
+-- Add jj as an alternative to Esc key
+map("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
