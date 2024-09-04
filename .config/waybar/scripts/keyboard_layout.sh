d760689521fa121eaa7ec38a8f222ef092a79814
zeyadsleem <zeyadsleem1@gmail.com>
2024-09-04 13:33:52 +0300

    back to i3 config

--- .config/waybar/scripts/keyboard_layout.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-layout=$(swaymsg -t get_inputs | jq -r '.[] | select(.type == "keyboard") | .xkb_active_layout_name' | head -n1)
-
-case ${layout,,} in
-"english") echo "🇺🇸 EN" ;;
-"arabic") echo "🇸🇦 AR" ;;
-*) echo "🌐 ${layout:0:2}" ;;
-esac
--- .zshrc
+++ .zshrc
@@ -234,3 +234,5 @@ export TERMINAL=kitty
 export QT_QPA_PLATFORMTHEME=qt5ct
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
+
+eval $(thefuck --alias)
