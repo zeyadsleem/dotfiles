87cd74844674fa2fe0f7a2e2d7d3e15a414add59
zeyadsleem <zeyadsleem1@gmail.com>
2025-06-01 20:35:36 +0300

    add Iosevka font to kitty setup

---
+++ .config/nvim/lua/plugins/php.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "php", "blade" })
+
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      if not parser_config.blade then
+        parser_config.blade = {
+          install_info = {
+            url = "https://github.com/EmranMR/tree-sitter-blade",
+            files = { "src/parser.c" },
+            branch = "main",
+          },
+          filetype = "blade",
+        }
+      end
+    end,
+  },
+}
