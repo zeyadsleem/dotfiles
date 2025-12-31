5283983e397e2b85e4d9c53308876fcbbc12179f
zeyad sleem <zeyadsleem1@gmail.com>
2025-12-31 15:04:07 +0200

    update lazyvim and lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,16 +7,12 @@ return {
         section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
         lualine_y = {
           { "progress", separator = " ", padding = { left = 1, right = 0 } },
           { "location", padding = { left = 0, right = 1 } },
         },
         lualine_z = {
           {
-            function()
-              return " "
-            end,
             padding = { left = 0, right = 0 },
             separator = { right = "" },
           },
