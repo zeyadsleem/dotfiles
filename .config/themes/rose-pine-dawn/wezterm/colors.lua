b8b55fdc56fe7572174c8f3f319c7475c78ebe9d
zeyad sleem <zeyadsleem1@gmail.com>
2026-01-25 17:37:52 +0200

    feat: implement omarchy-style theme switching system

---
+++ .config/themes/rose-pine-dawn/wezterm/colors.lua
@@ -0,0 +1,18 @@
+return {
+  foreground = '#575279',
+  background = '#faf4ed',
+  cursor_bg = '#575279',
+  cursor_border = '#575279',
+  cursor_fg = '#faf4ed',
+  selection_bg = '#6e6a86',
+  selection_fg = '#575279',
+  
+  ansi = {
+    '#f2e9e1', '#b4637a', '#286983', '#ea9d34',
+    '#56949f', '#907aa9', '#d7827e', '#575279',
+  },
+  brights = {
+    '#9893a5', '#b4637a', '#286983', '#ea9d34',
+    '#56949f', '#907aa9', '#d7827e', '#575279',
+  },
+}
---
+++ .config/themes/rose-pine-dawn/zsh/env.zsh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="
+  --color=fg:#575279,bg:#faf4ed,hl:#d7827e
+  --color=fg+:#575279,bg+:#f2e9e1,hl+:#d7827e
+  --color=info:#56949f,prompt:#907aa9,pointer:#d7827e
+  --color=marker:#b4637a,spinner:#ea9d34,header:#286983
+"
