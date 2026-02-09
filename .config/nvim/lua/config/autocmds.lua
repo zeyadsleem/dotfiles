69df18a56c59abaf40a3d5386043f2226b1495e6
zeyad sleem <zeyadsleem1@gmail.com>
2026-02-09 13:28:13 +0200

    feat(nvim): use icons for diagnostics instead of text labels

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -16,7 +16,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
     -- Disable virtual text for this buffer
     vim.diagnostic.config({
       virtual_text = false,
-      signs = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.HINT] = " ",
+          [vim.diagnostic.severity.INFO] = " ",
+        },
+      },
       underline = true,
       update_in_insert = false,
       severity_sort = true,
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -48,3 +48,15 @@ vim.cmd([[
 ]])
 
 vim.g.lazyvim_python_ruff = "ruff"
+
+-- Diagnostic signs
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
+})
