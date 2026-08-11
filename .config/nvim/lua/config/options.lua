a1a8108febba09970f26bf183cc511ea66db8e6d
zeyad sleem <zeyadsleem1@gmail.com>
2026-08-11 18:17:25 +0300

    feat(nvim): enable test and mini-hipatterns extras, autoindent and brace expansion

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@ opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.clipboard = "unnamedplus"
+opt.autoindent = true
 
 opt.termguicolors = true
 opt.termbidi = true
@@ -13,6 +14,25 @@ opt.fileencoding = "utf-8"
 opt.conceallevel = 0
 opt.concealcursor = ""
 opt.signcolumn = "yes:2"
+
+vim.keymap.set("i", "<CR>", function()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local line = vim.api.nvim_get_current_line()
+  if col >= 1 and line:sub(col, col) == "{" and line:sub(col + 1, col + 1) == "}" then
+    local base_indent = vim.fn.indent(row)
+    local inner = base_indent + vim.bo.shiftwidth
+    local before = line:sub(1, col - 1)
+    local after = line:sub(col + 2)
+    vim.api.nvim_buf_set_lines(0, row - 1, row, false, {
+      before .. "{",
+      string.rep(" ", inner),
+      string.rep(" ", base_indent) .. "}" .. after,
+    })
+    vim.api.nvim_win_set_cursor(0, { row + 1, inner })
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
+  end
+end, { desc = "Expand braces on Enter" })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter", "FileType" }, {
   callback = function()
